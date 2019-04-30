7e29301522d837f1078907ce197dc00592ac0d02
byambaa005 <Aemon@123>
2019-04-30 12:06:52 -0500

    promise

--- homework01.js
+++ homework01.js
@@ -23,30 +23,28 @@
     function ex02() {
         String.prototype.filterWords = function (strArr) {
 
-            const origArr = this.split(" ");
+            let origArr = this.split(" ");
             const starStr = "***";
 
-            origArr.forEach(function (value, index) {
-                if (strArr.includes(value)) {
-                    origArr[index] = starStr;
+            origArr = origArr.map((value, index) => {
+                if(strArr.includes(value)) {
+                    origArr[index] = starStr
                 }
+                console.log(origArr);
             });
-            return new Promise(function (resolve, reject) {
-                if (strArr.includes(value)) {
-                    resolve();
-                } else {
-                    reject();
-                }
-            }).then(function (result) {
-                console.log("Promise returned: " + result);
-            }).catch(function (err) {
-                console.log(err);
-            });
-        };
 
+            // return new Promise(function (resolve, reject) {
+            //         resolve(newArr);
+            // }).then(function (result) {
+            //     console.log("Promise returned: " + result);
+            // }).catch(function (err) {
+            //     console.log(err);
+            // });
+        };
+        console.log("This house is nice!".filterWords(['house', 'nice!']))
     }
-    ex01();
-    // ex02();
+    // ex01();
+    ex02();
     // ex03();
 
 }());
