8799e46b76f07fc9c7991cd6429f0ee3afddbb80
DmaC <demon.005.gl@gmail.com>
2019-05-08 21:34:25 -0500

    homework06

--- homework_06/app.js
+++ homework_06/app.js
@@ -3,12 +3,17 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let helmet = require('helmet');
+let morgan = require('morgan');
+let fs = require('fs');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 let gradesRouter = require('./routes/grades');
 
 let app = express();
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,9 +25,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.disable("x-powered-by");
+app.enable('trust proxy');
+app.enable("strict routing");
+app.enable('case sensitive routing');
+app.use(helmet());
+app.use(morgan('combined', { stream: accessLogStream }));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/grades', gradesRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- homework_06/routes/grades.js
+++ homework_06/routes/grades.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
 const R = require('ramda');
+const jsonParser = express.json();
 
 let gradeDataRaw = [
     {id: 1, name: "Asaad Saad", course: "CS572", grade: 95},
@@ -8,22 +9,30 @@ let gradeDataRaw = [
     {id: 3, name: "Demo Duka", course: "CS572", grade: 93}
 ];
 
-let gradeDataJSON = JSON.stringify(gradeDataRaw);
 
 const eqByUserId = (id)=>  R.propEq('id', id);
 const filterUserId = (id, data) => R.filter(eqByUserId(id), data);
 
 router.get('/', function (req, res, next) {
-    res.status(200).json(gradeDataJSON);
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.status(200).json(gradeDataRaw);
 });
 
 router.get('/:id', function (req, res, next) {
     res.status(200).json(filterUserId(parseInt(req.params.id), gradeDataRaw)[0]);
 });
 
-router.post('/', function (req, res, next) {
+router.post('/', jsonParser, function (req, res, next) {
     gradeDataRaw.push(req.body);
-    res.status(200).send("Successfully saved following object" + req.body);
+    res.status(200).send("Successfully saved following object");
+});
+
+router.delete('/:id', function (req, res, next) {
+    let curUser = filterUserId(parseInt(req.params.id), gradeDataRaw)[0];
+    if (gradeDataRaw.indexOf(curUser) > -1) {
+        gradeDataRaw.splice(gradeDataRaw.indexOf(curUser), 1);
+    }
+    res.status(200).json("Successfully removed following object");
 });
 
 module.exports = router;
